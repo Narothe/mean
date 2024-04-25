b90e8937b050b638b6ddb336e44f0e31d6c7197f
Narothe <kuba.dojka@gmail.com>
2024-04-25 23:16:06 +0200

    feat: before tasks

--- api/lib/config.ts
+++ api/lib/config.ts
@@ -1,5 +1,7 @@
-export const config = {
+export let config: { JwtSecret: string; port: string | number; supportedPostCount: number; databaseUrl: string };
+config = {
     port: process.env.PORT || 3100,
     supportedPostCount: 15,
-    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/IoT?retryWrites=true&w=majority'
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/IoT?retryWrites=true&w=majority',
+    JwtSecret: 'secret'
 };
--- api/lib/index.ts
+++ api/lib/index.ts
@@ -2,8 +2,10 @@ import App from './app';
 import IndexController from "./controllers/index.controller";
 import DataController from "./controllers/data.controller";
 import PostController from "./controllers/post.controller";
+import UserController from "./controllers/user.controller";
 
 const app: App = new App([
+    new UserController(),
     new IndexController(),
     new DataController(),
     new PostController()
