fc7555100231f267b9aded73e5ca69478dacbc9a
Narothe <kuba.dojka@gmail.com>
2024-06-20 22:51:57 +0200

    feat: implements popup in login

--- new-mean-angular/Lab11_1_TAW-master/src/app/app.config.ts
+++ new-mean-angular/Lab11_1_TAW-master/src/app/app.config.ts
@@ -5,12 +5,18 @@ import {HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors} from 
 import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
 import {authInterceptor} from "./services/auth/auth.interceptor";
+import {importProvidersFrom} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    importProvidersFrom(BrowserAnimationsModule, ToastModule),
+    MessageService
   ]
 };
--- new-mean-angular/Lab11_1_TAW-master/src/app/components/login/login.component.ts
+++ new-mean-angular/Lab11_1_TAW-master/src/app/components/login/login.component.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from "../../services/auth.service";
-import {Router} from "@angular/router";
-import {FormsModule} from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { MessageService } from 'primeng/api';
+import {ToastModule} from "primeng/toast";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ToastModule],
   providers: [AuthService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -22,18 +24,20 @@ export class LoginComponent implements OnInit {
   public logout?: boolean;
 
   constructor(public authService: AuthService,
-              private router: Router) {
-  }
+              private router: Router,
+              private messageService: MessageService) { }
 
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   signIn() {
     return this.authService.authenticate(this.credentials).subscribe((result) => {
       if (!result) {
         this.logged = false;
+        console.error('Błędne logowanie: nieprawidłowy login lub hasło.');
+        this.messageService.add({ severity: 'error', summary: 'Błąd logowania', detail: 'Nieprawidłowy login lub hasło.' });
       } else {
+        console.log('Zalogowano pomyślnie.');
+        this.messageService.add({ severity: 'success', summary: 'Sukces', detail: 'Zalogowano pomyślnie.' });
         this.logout = false;
         this.credentials = {
           login: '',
@@ -41,7 +45,9 @@ export class LoginComponent implements OnInit {
         };
         this.router.navigate(['/']);
       }
+    }, (error) => {
+      console.error('Wystąpił błąd podczas logowania:', error);
+      this.messageService.add({ severity: 'error', summary: 'Błąd', detail: 'Wystąpił błąd podczas logowania.' });
     });
   }
-
 }
